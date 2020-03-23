bf75be4cad543f9dcd6140b3b2f13a8a7ea8e908
Eugene_Z <evgeniy.zolotarev@firstlinesoftware.com>
2020-03-23 15:57:30 +0300

    initial

---
+++ precache-manifest.87c662c5545b337e57732c89dcdf479a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "430e36a140887f8124ffd21d10212059",
+    "url": "./index.html"
+  },
+  {
+    "revision": "39b76f3dd3ba7d9cbb53",
+    "url": "./static/css/main.5552860e.chunk.css"
+  },
+  {
+    "revision": "c5eff83314471c9fc0bf",
+    "url": "./static/js/2.d32afe4f.chunk.js"
+  },
+  {
+    "revision": "39b76f3dd3ba7d9cbb53",
+    "url": "./static/js/main.8e0027d1.chunk.js"
+  },
+  {
+    "revision": "23f72fe3308ab4ab991a",
+    "url": "./static/js/runtime-main.e0ac0835.js"
+  },
+  {
+    "revision": "5d9df3cd46fe22ea399b382f24c9a90e",
+    "url": "./static/media/logo.5d9df3cd.png"
+  }
+]);
